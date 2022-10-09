14b2f3ddbcf7c1ce6b023a5f9b249af2f0a651f1
Sohan Sarabu <sohan.sarabu@gmail.com>
2022-10-09 16:33:36 -0400

    index api function

---
+++ pages/api/index.js
@@ -0,0 +1,12 @@
+export default function handler (req, res) {
+  let json
+  if (req.method === 'POST') {
+    // Process a POST request
+    json = req.body
+    console.log(json)
+  } else {
+    // Handle any other HTTP method
+    console.log('sending ')
+    res.status(200).json(json)
+  }
+}
