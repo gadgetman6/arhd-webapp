876b222a749e0ad9ab231129ee008d2b508dd2f6
Sohan Sarabu <sohan.sarabu@gmail.com>
2022-10-09 17:10:45 -0400

    wd

--- pages/api/main.js
+++ pages/api/main.js
@@ -1,13 +1,16 @@
-export default function handler (req, res) {
-  let json = { base: 'test' }
+import fs from 'fs'
+
+export default async function handler (req, res) {
   if (req.method === 'POST') {
     // Process a POST request
-    json = req.body
-    console.log(json)
-    res.status (200)
+    let json = req.body
+    fs.writeFileSync(`./public/file.json`, JSON.stringify(json))
+    res.status(200).json({ status: 'success' })
   } else {
     // Handle any other HTTP method
     console.log('sending ')
+    var json = fs.readFileSync('./public/file.json')
+    json = JSON.parse (json)
     res.status(200).json(json)
   }
 }
